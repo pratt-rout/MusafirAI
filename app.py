e385542b542308ba6f764dbb4ebd59caf5b58661
PraTT <pratt.rout@gmail.com>
2025-09-21 18:19:21 +0530

    Add Login

--- app.py
+++ app.py
@@ -2,11 +2,28 @@ import streamlit as st
 import datetime
 from dateutil.relativedelta import relativedelta
 
-from ai_helper import generate_plan
+from scripts.ai_helper import generate_plan
 
-st.set_page_config(page_title="MusafirAI", page_icon="Musafir.png", layout="wide")
+# if not st.user.is_logged_in:
+#     if st.button("Log in with Google"):
+#         st.login()
+#         st.markdown("If you are not redirected automatically, please refresh the page.")
+
+# st.markdown(f"Welcome! {st.user.name}")
+
+def login_screen():
+    st.header("This app is private.")
+    st.subheader("Please log in.")
+    st.button("Log in with Google", on_click=st.login)
+
+if not st.user.is_logged_in:
+    login_screen()
+else:
+    st.header(f"Welcome, {st.user.name}!")
+    st.button("Log out", on_click=st.logout)
+
+st.set_page_config(page_title="MusafirAI", page_icon="assets/Musafir.png", layout="wide")
 st.title("MusafirAI")
-
 
 col1, col2 = st.columns(2)
 
@@ -16,7 +33,7 @@ with col1:
     sample_end_date = start_date + relativedelta(days=10)
 
     date_range = st.date_input(
-        "Your Duration",
+        "Your Duration*",
         (start_date, sample_end_date),
         min_value = start_date,
         max_value = calendar_end_date,
@@ -25,36 +42,40 @@ with col1:
 
 with col1:
     theme = st.selectbox(
-        label="Your Theme",
+        label="Your Theme*",
         options=["Romantic", "Adventure", "Cultural", "Nature", "Relaxation"],
         index=1,
     )
 with col2:
     location = st.text_input(
-        "Your location (s)",
+        "Your location (s)*",
         placeholder = "E.g - Rome.   If multiple locations, separate by commas. Eg - Rome, Florence",
     )
 
 with col2:
     budget = st.select_slider(
-    "Your max budget",
+        "Your max budget*",
     options=list(range(1000,500100,100)),
     value=100000
 )
 
 special_request = st.text_input("Special requests (optional)", "", placeholder = "I want to include a spa day")
 
-if st.button("GENERATE ITINERARY", type="primary", width="stretch"):
-    output = generate_plan(
-        date_range=date_range, 
-        theme=theme, 
-        location=location, 
-        budget=budget, 
-        special_request=special_request
-    )
+if date_range and theme and location and budget:
+    if st.button("GENERATE ITINERARY", type="primary", width="stretch"):
+        output = generate_plan(
+            date_range=date_range, 
+            theme=theme, 
+            location=location, 
+            budget=budget, 
+            special_request=special_request
+        )
 
-    # st.markdown(output, unsafe_allow_html=True)
-    st.markdown(output)
+        # st.markdown(output, unsafe_allow_html=True)
+        st.markdown(output)
+
+# if st.button("Log out"):
+#     st.logout()
 
 
 
--- scripts/ai_helper.py
+++ scripts/ai_helper.py
@@ -32,8 +32,8 @@ def generate_plan(date_range, theme, location, budget, special_request):
             Show these expenses at top of the table , since it should be done before the trip starts. 
             Show final return flight towards the end of the table.
             Show Local Transport on the dates whenever its required. Do not club them towards the end.
-            Whenever you are mentioning any travel spot, show google link for that spot. Do not do it in the table, do it in the itinerary section.
-            Eg - [Location_Name](https://www.google.com/maps/search/?api=1&query=Location_Name)
+            Whenever you are mentioning any travel spot, show google link for that spot. Do not do it in the table, do it in the itinerary section. 
+            Use it in markdown format. Eg - [Location_Name](https://www.google.com/maps/search/?api=1&query=Location_Name)
 
             For visa application, if you have some valid links, you can provide them as well. Eg - [Visa Application](https://visa.vfsglobal.com/ind/en/ita/)
 
